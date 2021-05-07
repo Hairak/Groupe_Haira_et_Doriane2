6fc8fe75fd6648f0b1fc074791d393460489cc76
Hairak <77623242+Hairak@users.noreply.github.com>
2021-05-07 23:36:00 +0200

    Create video.js
    pour le stream

---
+++ video.js
@@ -0,0 +1,22 @@
+(function() {
+    var video = document.getElementById('video'),
+        vendorUrl = window.URL || window.webkitURL;
+
+// on source les dif media (chrome, moz, microsft)
+        navigator.getMedia = navigator.getUserMedia ||
+                         navigator.webkitGetUserMedia||   
+                         navigator.mozGetUserMedia ||
+                         navigator.msGetUserMedia; 
+                         
+// Pour capturer la video et le son : 
+navigator.getMedia ({
+    video: true,
+    audio: false
+}, function(stream) {
+    video.src = vendorUrl.createObjectURL(stream);
+    video.play();
+}, function(error){
+    //Une erreur est survenue
+    // error.code
+    });
+}) ();
